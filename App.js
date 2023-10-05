00316bc19db2874f3840812e3d04df01f3aef90f
Staicy Stadtler <staicy@hotmail.de>
2023-10-05 10:09:48 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SvgComponent from "./components/SvgComponent";
+import { SvgCircle } from "./components/SvgCircle";
+import { SvgMoveableCircle } from "./components/SvgMoveableCircle";
+import { PinchableCircle } from "./components/PinchableCircle";
+import { DeviceMotionExample } from "./components/DeviceMotionExample.js";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { DeviceMotionTest } from "./components/DeviceMotionTest";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        {/* <SvgComponent /> */}
+        {/* <SvgCircle /> */}
+        {/* <SvgMoveableCircle /> */}
+        {/* <PinchableCircle /> */}
+        <DeviceMotionExample />
+        {/* <DeviceMotionTest /> */}
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
